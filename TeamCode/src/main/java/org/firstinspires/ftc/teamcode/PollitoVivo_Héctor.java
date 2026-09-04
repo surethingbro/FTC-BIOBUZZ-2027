ae5411caa7b49177017456aa02f982c007e90b3f
Hector Fabregat <hectorfabregatn@gmail.com>
2026-09-04 15:49:26 -0500

    Pollito vivo de prueba

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PollitoVivo_Héctor.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PollitoVivo_Héctor {
+    //esto es una prueba bien papu pro
+
+    int conteo = 0;
+
+    for(int i; i= 10, 1++){
+        conteo = conteo + 1;
+    }
+
+
+}
